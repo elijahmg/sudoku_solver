9341d29064d47be2c28db6f0922d1df305b0a20d
Ilya <elijahmg25@gmail.com>
2021-02-06 22:32:01 +0100

    build

---
+++ build/dist/App.js
@@ -0,0 +1,9 @@
+import React from "../_snowpack/pkg/react.js";
+import Grid from "./components/Grid/Grid.js";
+import "./App.css.proxy.js";
+function App({}) {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "App"
+  }, /* @__PURE__ */ React.createElement(Grid, null));
+}
+export default App;
